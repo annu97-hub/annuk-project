f72f1ad1ee9860e73eb400ab3e5fdedf47c7df81
himanshu <himanshu064@gmail.com>
2021-06-27 13:49:32 +0530

    login - signup done

--- job_project/src/store/actions/auth.js
+++ job_project/src/store/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { DEFAULT_HEADERS, MULTIPART_FORM_DATA_HEADERS } from "../../utils";
-import { USER_SIGN_IN, USER_SIGN_OUT, USER_SIGN_UP } from "../constants/auth";
+import { DEFAULT_HEADERS } from "../../utils";
+import { USER_SIGN_IN, USER_SIGN_OUT } from "../constants/auth";
 import {
   LOGIN_USER_ENDPOINT,
   REGISTER_USER_ENDPOINT,
@@ -9,38 +9,13 @@ import {
 export const registerUser = (userData) => (dispatch) => {
   return new Promise(async (resolve, reject) => {
     try {
-      console.log(userData, 'userData');
-      // prepare form data
-      const formData = new FormData();
-      formData.append("firstName", userData.firstName);
-      formData.append("lastName", userData.lastName);
-      formData.append("email", userData.email);
-      formData.append("password", userData.password);
       
-      
-
       const { data } = await axios.post(
         `${REGISTER_USER_ENDPOINT}`,
         userData,
         DEFAULT_HEADERS
       );
 
-      console.log(data, "data as response from register");
-
-      // set the headers in axios
-      //axios.defaults.headers.Authorization = "Bearer " + data.token;
-
-      // // store the user and token in the localStorage
-      // localStorage.setItem("token", JSON.stringify(data.token));
-      // localStorage.setItem("user", JSON.stringify(data.user));
-
-      // dispatch({
-      //   type: USER_SIGN_IN,
-      //   payload: {
-      //     user: data.user,
-      //     token: data.token,
-      //   },
-      // });
       resolve();
     } catch (error) {
       reject(error);
@@ -56,19 +31,19 @@ export const loginUser = (userData) => (dispatch) => {
         userData,
         DEFAULT_HEADERS
       );
-
+      
       // set the headers in axios
-      axios.defaults.headers.Authorization = "Bearer " + data.token;
+      axios.defaults.headers.Authorization = "Bearer " + data.data.token;
 
       // store the user and token in the localStorage
-      localStorage.setItem("token", JSON.stringify(data.token));
-      localStorage.setItem("user", JSON.stringify(data.user));
+      localStorage.setItem("token", JSON.stringify(data.data.token));
+      localStorage.setItem("user", JSON.stringify(data.data.user));
 
       dispatch({
         type: USER_SIGN_IN,
         payload: {
-          user: data.user,
-          token: data.token,
+          user: data.data.user,
+          token: data.data.token,
         },
       });
       resolve();
